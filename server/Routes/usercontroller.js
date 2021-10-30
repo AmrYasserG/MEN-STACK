29b146c7b9a1e674e3b53e63e80d8e94190f240f
omar wahbi <omarashrafwahbi79@gmail.com>
2021-10-30 23:03:36 +0200

    inital setup

--- server/Routes/usercontroller.js
+++ server/Routes/usercontroller.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const cruds = require('../Cruds/cruds');
-const path = require("path");
 
 const routes = (app)=>{
-
-    app.use(express.static(path.join(__dirname, "../dist")));
 
     app.get("/test",(req,res)=>{
         res.send("Hello from home");
