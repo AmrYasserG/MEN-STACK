3462ab833a05bc657fb59b1043cd81c5ac587e05
omar wahbi <omarashrafwahbi79@gmail.com>
2021-11-02 22:46:51 +0200

    Created Admin Homepage FE

--- server/Routes/usercontroller.js
+++ server/Routes/usercontroller.js
@@ -6,9 +6,8 @@ const routes = (app)=>{
     app.get("/test",(req,res)=>{
         res.send("Hello from home");
     });
-
-    app.post("/addUser", cruds.addUser);  
-    app.get("/getAllFlights", cruds.getAllFlights);  
+ 
+    app.post("/addUser", cruds.addUser); 
 };
 
 module.exports = {routes};
