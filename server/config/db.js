bb951510cfbcb776676388e630e35395f22506be
Ahmed ElOraby <ahmed.eloraby3547@gmail.com>
2021-11-02 21:28:36 +0200

    First Backup

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 require("dotenv").config({ path: __dirname + "/./../../.env" });
-//const config = require("config");
-//const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
