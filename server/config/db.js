a2997fe704eae112637bb4ee75ebda6f0e9be3bd
Ahmed ElOraby <ahmed.eloraby3547@gmail.com>
2021-11-02 21:25:19 +0200

    First Backup

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
-const config = require("config");
-const db = config.get("mongoURI");
+require("dotenv").config({ path: __dirname + "/./../../.env" });
+//const config = require("config");
+//const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    const URI = process.env.MONGO_URI;
+    await mongoose.connect(URI, {
       useNewUrlParser: true,
     });
 
     console.log("MongoDB is Connected...");
   } catch (err) {
+    console.log(`connection string: ${process.env.MONGO_URL}`);
     console.error(err.message);
     process.exit(1);
   }
