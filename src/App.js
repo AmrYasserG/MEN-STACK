ca8872c1782ab54dfb8333bd2e410054e4bd387a
Mustafa <Muostafa>
2021-11-06 20:50:54 +0200

    create

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import CreateUser from './components/CreateUser/CreateUser';
 import ViewUsers from './components/ViewUsers/ViewUsers';
 import AdminHomepage from './components/AdminHomepage/AdminHomepage';
+import CreateFlight from './components/CreateFlight/CreateFlight';
 
 class App extends Component {
   render() {
@@ -12,7 +13,9 @@ class App extends Component {
         <div>
         <Route exact path='/' component={ViewUsers} />
         <Route path='/create-user' component={CreateUser} />
+        <Route path='/createFlight' component={CreateFlight} />
         <Route path='/AdminHomepage' component={AdminHomepage} />
+        <h1>dddd</h1>
         </div>
       </Router>
     );
