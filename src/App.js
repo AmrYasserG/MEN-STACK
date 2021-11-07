ab6d11f0825bbb8872a3216fd8abcc6e016ea03a
omar wahbi <omarashrafwahbi79@gmail.com>
2021-11-07 21:04:35 +0200

    pulling changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import AdminHomepage from './components/AdminHomepage/AdminHomepage';
+import scratch from './components/scratch/scratch';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
         <div>
         <Route exact path='/' component={AdminHomepage} />
         <Route path='/AdminHomepage' component={AdminHomepage} />
+        <Route path='/scratch' component={scratch} />
         </div>
       </Router>
     );
