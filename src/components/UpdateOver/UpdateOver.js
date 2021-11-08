5a64be83eeb2394fe22d4625cd06b3251ea0eaf7
Ahmed ElOraby <ahmed.eloraby3547@gmail.com>
2021-11-08 23:57:54 +0200

    Final

--- src/components/UpdateOver/UpdateOver.js
+++ src/components/UpdateOver/UpdateOver.js
@@ -1,16 +1,22 @@
-import * as React from 'react'
+import * as React from "react";
 import Button from "@mui/material/Button";
-import './UpdateOver.css';
+import "./UpdateOver.css";
 
 function UpdateOver(props) {
-    return (props.trigger) ? (
-        <div className="upd">
-            <div className="upd-inner">
-             { props.children }
-            <Button variant="contained" className="close-btn" onClick={()=> props.setTrigger(false)}>Cancel</Button>
-            </div>
-        </div>
-    ) : "";
+  return props.trigger ? (
+    <div className="upd">
+      <div className="upd-inner">{props.children}</div>
+      <Button
+        variant="contained"
+        className="close-btn"
+        onClick={() => props.setTrigger(false)}
+      >
+        Cancel
+      </Button>
+    </div>
+  ) : (
+    ""
+  );
 }
 
-export default UpdateOver
+export default UpdateOver;
